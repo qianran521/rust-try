a27d261bd6e44485e4c65eb7da30f9dd3d6b7bb3
qianran <qianran521@gmail.com>
2026-04-10 18:52:14 +0000

    2

--- hello_world/src/main.rs
+++ hello_world/src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
+    println!("Fibonacci sequence:");
+    for i in 0..10 {
+        println!("{}: {}", i, fibonacci(i));
+    }
+}
+
+fn fibonacci(n: u32) -> u32 {
+    if n <= 1 {
+        n
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
 }
